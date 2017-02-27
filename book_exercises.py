7f42da686595010410fea1e426c642a377063c6e
JoeFleck05 <joefleck05@gmail.com>
2017-02-27 13:36:50 -0500

    continued practice

--- book_exercises.py
+++ book_exercises.py
@@ -25,3 +25,44 @@ def even_odd():
         print(n, " is even")
     else:
         print(n, " is odd")
+
+def f():
+    x = 1
+    y = 2
+    z = 3
+    print(x)
+    print(y)
+    print(z)
+
+
+try:
+    a = input("type a number: ")
+    b = input("type another: ")
+    a = int(a)
+    b = int(b)
+    print(a/b)
+except(ZeroDivisionError,ValueError):
+    print("Invalid Input.")
+
+
+def squared_function(n):
+    print(n*2)
+
+def string_print(str):
+    print(str)
+
+'''
+4. Write a program with two functions. The first function should take an
+integer as a parameter and return the result of the integer divided by 2.
+The second function should take an integer as a parameter and return the
+result of the integer multiplied by 4. Call the first function, save the
+result as a variable, and pass it as a parameter to the second function.
+'''
+
+def takes_int():
+    a = input("I need a number:")
+    int_return = int(a)
+    return int_return/2
+def times_four(n):
+    return n * 4
+
